ca8dd8db680e50f658ec26e2c6fde2dadcd63a35
Zahidul Haque <zahidulhaquer@gmail.com>
2025-09-29 04:01:21 +0600

    Project is completed 09-29-2025

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import Entry from "./components/Entry"
 import data from "./data"
 
 export default function App() {
-    
+
     const entryElements = data.map((entry) => {
         return (
             <Entry
-                img={entry.img}
-                title={entry.title}
-                country={entry.country}
-                googleMapsLink={entry.googleMapsLink}
-                dates={entry.dates}
-                text={entry.text}
+                key={entry.id}
+                {...entry}
+                // entry={entry}
+                // img={entry.img}
+                // title={entry.title}
+                // country={entry.country}
+                // googleMapsLink={entry.googleMapsLink}
+                // dates={entry.dates}
+                // text={entry.text}
             />
         )
     })
-    
+
     return (
         <>
             <Header />
